203e69277e156ccac0d8e8cbb7197f5deef9edaf
hujiaju <nanhujiaju@gmail.com>
2018-09-06 15:59:13 +0800

    tmp done for cordova

---
+++ SBSwiftComponents/SBCordova/CordovaProfile.swift
@@ -0,0 +1,9 @@
+//
+//  CordovaProfile.swift
+//  SBSwiftComponents
+//
+//  Created by nanhu on 2018/9/6.
+//  Copyright © 2018年 nanhu. All rights reserved.
+//
+
+import Foundation
---
+++ SBSwiftComponents/SBCordova/KitsCordova.swift
@@ -0,0 +1,9 @@
+//
+//  KitsCordova.swift
+//  SBSwiftComponents
+//
+//  Created by nanhu on 2018/9/6.
+//  Copyright © 2018年 nanhu. All rights reserved.
+//
+
+import Foundation
