ef4cd145c12303e4eecf4d3c11d43349bd7ea460
hujiaju <nanhujiaju@gmail.com>
2018-09-12 14:56:26 +0800

    add browser

---
+++ SBSwiftComponents/SBBrowser/Activities.swift
@@ -0,0 +1,9 @@
+//
+//  Activities.swift
+//  SBSwiftComponents
+//
+//  Created by nanhu on 2018/9/12.
+//  Copyright © 2018年 nanhu. All rights reserved.
+//
+
+import Foundation
---
+++ SBSwiftComponents/SBBrowser/Browser.swift
@@ -0,0 +1,9 @@
+//
+//  Browser.swift
+//  SBSwiftComponents
+//
+//  Created by nanhu on 2018/9/12.
+//  Copyright © 2018年 nanhu. All rights reserved.
+//
+
+import Foundation
