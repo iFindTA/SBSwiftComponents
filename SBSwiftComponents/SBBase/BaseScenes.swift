d1437424ec819b49bd65f648961184b4ec9b665f
hujiaju <nanhujiaju@gmail.com>
2018-10-16 18:29:02 +0800

    update

--- SBSwiftComponents/SBBase/BaseScenes.swift
+++ SBSwiftComponents/SBBase/BaseScenes.swift
@@ -218,3 +218,90 @@ public class BaseNavigationBar: UINavigationBar {
         
     }
 }
+
+// MARK: - mask 弹出遮罩 base
+class MaskBaseScene: BaseScene {
+    /// vars
+    private var fatherScene: UIView?
+    private var topOffset: CGFloat = 0
+    private var availableHeight: CGFloat = 0///info scene
+    private var whetherDisplay: Bool = false
+    
+    /// lazy vars
+    public lazy var scene: BaseScene = {
+        let s = BaseScene(frame: .zero)
+        return s
+    }()
+    public lazy var closeBtn: BaseButton = {
+        let color = AppColor.COLOR_TITLE_GRAY
+        let font = AppFont.iconFont(AppFont.SIZE_LARGE_TITLE)
+        let b = BaseButton(type: .custom)
+        b.titleLabel?.font = font
+        b.setTitleColor(color, for: .normal)
+        b.setTitle("\u{e605}", for: .normal)
+        b.addTarget(self, action: #selector(dismiss), for: .touchUpInside)
+        return b
+    }()
+    
+    /// init
+    class func mask(_ father: UIView, top offset: CGFloat, with bottom: CGFloat=0) -> MaskBaseScene {
+        return MaskBaseScene(father, top: offset, with: bottom)
+    }
+    private init(_ father: UIView, top offset: CGFloat, with bottom: CGFloat=0) {
+        super.init(frame: .zero)
+        fatherScene = father
+        topOffset = offset
+        availableHeight = AppSize.HEIGHT_SCREEN - offset - bottom
+        /// custom
+        isHidden = true
+        backgroundColor = ClearBgColor
+        father.addSubview(self)
+        self.snp.makeConstraints { (m) in
+            m.edges.equalToSuperview()
+        }
+        /// subviews
+        addSubview(scene)
+        scene.addSubview(closeBtn)
+        let of = fetchEndFrame()
+        scene.frame = of
+        configureSubviews()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    /// getter
+    private func fetchEndFrame() -> CGRect {
+        let top = whetherDisplay ? topOffset : AppSize.HEIGHT_SCREEN
+        return CGRect(x: 0, y: top, width: AppSize.WIDTH_SCREEN, height: availableHeight)
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        closeBtn.snp.makeConstraints { (m) in
+            m.top.equalToSuperview().offset(HorizontalOffset)
+            m.right.equalToSuperview().offset(-HorizontalOffset)
+        }
+    }
+    public func configureSubviews() {}/// should be implemented by sub-class
+    public func updateDisplay() {}/// should be implemented by sub-class
+    
+    /// events
+    public func show() {
+        whetherDisplay.toggle()
+        isHidden = false
+        updateDisplay()
+        fatherScene?.bringSubview(toFront: self)
+        updateScenePosition()
+    }
+    @objc private func dismiss() {
+        whetherDisplay.toggle()
+        updateScenePosition()
+    }
+    private func updateScenePosition() {
+        let to = fetchEndFrame()
+        UIView.animate(withDuration: Macros.APP_ANIMATE_INTERVAL, animations: {[weak self] in
+            self?.scene.frame = to
+        }) { [weak self](f) in
+            self?.isHidden = !(self?.whetherDisplay ?? true)
+        }
+    }
+}
